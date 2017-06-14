383d9a76f643472c3596abdcc4192757eebe6b19
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air.local>
2017-06-14 16:16:48 -0700

    day 8

--- Day7/demo7.rb/demo7.rb
+++
@@ -1,2 +0,0 @@
-# bc_cities_population = {vancouver: 2135201, victoria:  316327, abbotsford: 149855, kelowna: 141767, nanaimo:  88799, white_rock: 82368, kamloops: 73472, chilliwack: 66382 }
-# Write a method that takes the hash and prints if city is large (more than 100,000) or small (otherwise). Printing something like: Vancouver is a large city
