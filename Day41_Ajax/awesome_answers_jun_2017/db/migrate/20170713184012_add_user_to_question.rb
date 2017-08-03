12ebb9934d265a4c3dbb1b7ea07094d62db36af9
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air.local>
2017-08-03 13:44:58 -0700

    add ajax

--- Day41_Ajax/awesome_answers_jun_2017/db/migrate/20170713184012_add_user_to_question.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToQuestion < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :questions, :user, foreign_key: true, index: true
-  end
-end
--- Day41_Ajax/awesome_answers_jun_2017/db/migrate/20170713184127_add_user_to_answer.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToAnswer < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :answers, :user, foreign_key: true, index: true
-  end
-end
